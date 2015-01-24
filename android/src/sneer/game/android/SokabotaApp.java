c994df0fe9a91885d0a637b92ba0a82226cdc175
Rodrigo B. de Oliveira <rbo@acm.org>
2015-01-24 16:19:11 -0200

    fix android compilation

---
+++ android/src/sneer/game/android/SokabotaApp.java
@@ -0,0 +1,38 @@
+package sneer.game.android;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+public class SokabotaApp implements ApplicationListener {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render() {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
