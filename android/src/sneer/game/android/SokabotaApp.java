732fc8368f61e849757068566487cf9ae8a798a9
Rodrigo B. de Oliveira <rbo@acm.org>
2015-01-24 17:13:43 -0200

    render random tiles to start with

--- android/src/sneer/game/android/SokabotaApp.java
+++ android/src/sneer/game/android/SokabotaApp.java
@@ -1,38 +1,74 @@
 package sneer.game.android;
 
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 
-public class SokabotaApp implements ApplicationListener {
+public class SokabotaApp extends ApplicationAdapter {
+    private TiledMap map;
+    private TiledMapRenderer renderer;
+    private OrthographicCamera camera;
+    private Texture tiles;
+    private BitmapFont font;
+    private SpriteBatch batch;
     @Override
-    public void create() {
+    public void create () {
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, (w / h) * 320, 320);
+        camera.update();
+
+        font = new BitmapFont();
+        batch = new SpriteBatch();
+
+        {
+            tiles = new Texture(Gdx.files.internal("todos.png"));
+            TextureRegion[][] splitTiles = TextureRegion.split(tiles, 16, 16);
+            map = new TiledMap();
+            MapLayers layers = map.getLayers();
+            for (int l = 0; l < 10; l++) {
+                TiledMapTileLayer layer = new TiledMapTileLayer(150, 100, 16, 16);
+                for (int x = 0; x < 150; x++) {
+                    for (int y = 0; y < 100; y++) {
+                        int ty = (int)(Math.random() * splitTiles.length);
+                        int tx = (int)(Math.random() * splitTiles[ty].length);
+                        TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
+                        cell.setTile(new StaticTiledMapTile(splitTiles[ty][tx]));
+                        layer.setCell(x, y, cell);
+                    }
+                }
+                layers.add(layer);
+            }
+        }
+
+        renderer = new OrthogonalTiledMapRenderer(map);
 
     }
 
     @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void render() {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void dispose() {
-
+    public void render () {
+        Gdx.gl.glClearColor(100f / 255f, 100f / 255f, 250f / 255f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
+        batch.begin();
+        font.draw(batch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 10, 20);
+        batch.end();
     }
 }
