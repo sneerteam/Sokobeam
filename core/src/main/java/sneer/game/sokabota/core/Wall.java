5f3a5c31e2f874f71e5c54c47a37abbe4fbdbf6f
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-01-24 20:31:03 -0200

    Walls cannot be pushed.

--- core/src/main/java/sneer/game/sokabota/core/Wall.java
+++ core/src/main/java/sneer/game/sokabota/core/Wall.java
@@ -1,9 +1,15 @@
 package sneer.game.sokabota.core;
 
+import sneer.gameengine.grid.Direction;
 import sneer.gameengine.grid.Thing;
 
 public class Wall extends Thing {
 
+	@Override
+	public boolean push(Direction dir) {
+		return false;
+	}
+	
 	@Override
 	public String toString() {
 		return "W";
